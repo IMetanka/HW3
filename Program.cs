b083a74ed27f56e49497632f89f8f881bd2c468c
IMetanka <98908315+IMetanka@users.noreply.github.com>
2022-04-03 11:53:46 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите целое число:");
+            int Number = int.Parse(Console.ReadLine());
+            bool flag = true;
+            int i = 2;
+
+
+            while (Number % i == 0)
+            {
+                flag = false;
+                i++;
+                break;
+            }
+
+            while (Number % i != 0)
+            {
+                flag = true;
+                i++;
+                break;
+            }
+
+
+
+            //for (int i = 2; i <= Number; i++)
+            //{
+            //    if (Number % i != 0)
+            //        N = true;
+
+            //    else 
+            //        N = false;
+
+
+            //}
+
+            if (flag = true)
+            Console.WriteLine($"Число простое.");
+            else
+            Console.WriteLine($"Число не является простым.");
+
+
+            Console.ReadKey();
+
+
+
+            
+
+        }
+    }
+}
